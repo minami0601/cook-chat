e546eb910a9d20228acea6a61a0429a73b6e93a6
minami0601 <minami.love.bb@icloud.com>
2020-07-19 20:32:42 +0900

    commit

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,14 @@
+class LikesController < ApplicationController
+    def create
+        @like = current_user.likes.create(recipe_id: params[:recipe_id])
+        redirect_back(fallback_location: root_path)
+    end
+
+    def destroy
+        @recipe = Recipe.find(params[:recipe_id])
+        @like = current_user.likes.find_by(recipe_id: @recipe.id)
+        @like.destroy
+        redirect_back(fallback_location: root_path)
+    end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_10_135010) do
+ActiveRecord::Schema.define(version: 2020_07_16_101625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "recipe_id"
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "recipe_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "body"
+    t.string "image_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
